541e1a41faf416e7a2bc2aab8873d5056bdd7a57
Leo Wilkin <leo@wilkin.xyz>
2026-03-22 12:13:54 +0000

    v1 docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,56 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightKbd from 'starlight-kbd'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [
+				starlightKbd({
+				types: [
+					{ id: 'windows', label: 'Windows', default: true },
+				],
+				}),
+			],
+			title: 'Jupyter Classroom',
+			logo: {
+				src: './src/assets/favicon.svg',
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/leowilkin/jupyter-classroom' }],
+			editLink: {
+				baseUrl: 'https://github.com/leowilkin/jupyter-classroom-docs/edit/main/',
+			},
 			sidebar: [
+				{ label: 'GitHub Repo', link: 'https://github.com/leowilkin/jupyter-classroom' },
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+				label: 'Configuration',
+				items: [
+					'configuration',
+					'configuration/installation',
+					'configuration/access',
+					{
+						label: 'OAuth Providers',
+						items: [
+							'configuration/oauth/azure',
+							'configuration/oauth/github',
+							'configuration/oauth/google',
+							'configuration/oauth/custom',
+						],
+					},
+					'configuration/lessons',
+					'configuration/classroom'
+				],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Usage',
+					autogenerate: { directory: 'usage' },
 				},
 			],
+			components: {
+				Footer: './src/components/Footer.astro',
+			},
 		}),
 	],
 });
